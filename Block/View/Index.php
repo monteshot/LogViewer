b509304fd4794675930a722de32bb7ab870509a8
Mathew Shaw <mathew@sozodesign.co.uk>
2020-01-30 15:04:22 +0000

    Changed to allow more lines in preview

--- Block/View/Index.php
+++ Block/View/Index.php
@@ -1,38 +1,58 @@
 <?php
 namespace PhilTurner\LogViewer\Block\View;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\View\Element\Template\Context;
+use PhilTurner\LogViewer\Helper\Data;
+
 class Index extends \Magento\Framework\View\Element\Template
 {
     /**
-     * @var \PhilTurner\LogViewer\Helper\Data
+     * @var Data
      */
     protected $logDataHelper;
+    protected $scopeConfig;
+    const XML_PATH_LINES = 'dev/logviewer/lines';
+
 
     /**
-     * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \PhilTurner\LogViewer\Helper\Data $logDataHelper
+     * Index constructor.
+     * @param ScopeConfigInterface $scopeConfig
+     * @param Context $context
+     * @param Data $logDataHelper
      * @param array $data
      */
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \PhilTurner\LogViewer\Helper\Data $logDataHelper,
+        ScopeConfigInterface $scopeConfig,
+        Context $context,
+        Data $logDataHelper,
         array $data = []
-    )
-    {
+    ) {
+        $this->scopeConfig = $scopeConfig;
         $this->logDataHelper = $logDataHelper;
         parent::__construct($context, $data);
     }
 
+    /**
+     * @return string
+     */
     public function getLogFile()
     {
+        $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+        $lines = $this->scopeConfig->getValue(self::XML_PATH_LINES, $storeScope);
+        if ($lines == 0) {
+            $lines = 20;
+        }
         $params = $this->_request->getParams();
-        return $this->logDataHelper->getLastLinesOfFile($params[0], 10);
+        return $this->logDataHelper->getLastLinesOfFile($params[0], $lines);
     }
 
+    /**
+     * @return mixed
+     */
     public function getFileName()
     {
         $params = $this->_request->getParams();
         return $params[0];
     }
-
 }
